11a8db3dc672bd4547b50d5c59e2c629d7bb6b56
Will Kupec <willem.kupec@gmail.com>
2024-06-11 11:18:29 +0200

    [webapp] created header component

--- CampusEventApp/CampusEventWebApp/webapp/src/index.js
+++ CampusEventApp/CampusEventWebApp/webapp/src/index.js
@@ -1,11 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import App from "./App"
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route>
+          <Home path="/"/>
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>
 )
---
+++ CampusEventApp/CampusEventWebApp/webapp/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+import Header from "../components/Header"
+
+const Home = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default Home
