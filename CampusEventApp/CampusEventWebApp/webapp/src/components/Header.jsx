7c66da2e35f8be29b200347de430aee5b9be0984
Will Kupec <willem.kupec@gmail.com>
2024-06-16 22:29:28 +0200

    [webapp_events] updated header

--- CampusEventApp/CampusEventWebApp/webapp/src/components/Header.jsx
+++ CampusEventApp/CampusEventWebApp/webapp/src/components/Header.jsx
@@ -1,100 +1,34 @@
 import * as React from "react"
 import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
 import Toolbar from "@mui/material/Toolbar"
-import IconButton from "@mui/material/IconButton"
-import Typography from "@mui/material/Typography"
-import Menu from "@mui/material/Menu"
-import Container from "@mui/material/Container"
-import Avatar from "@mui/material/Avatar"
-import Button from "@mui/material/Button"
-import Tooltip from "@mui/material/Tooltip"
-import MenuItem from "@mui/material/MenuItem"
 import { Link } from "react-router-dom"
-
-const settings = ["Profile", "Account", "Dashboard", "Logout"]
+import { Avatar, Stack } from "@mui/material"
 
 const Header = () => {
-  const [anchorElUser, setAnchorElUser] = React.useState(null)
-
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget)
-  }
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null)
-  }
-
   return (
-    <AppBar position="static" sx={{ backgroundColor: "#9fe68b" }}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Button component={Link} to="/">
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              sx={{
-                mr: 8,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                color: "white",
-                textDecoration: "none",
-              }}
-            >
-              HTW Helper
-            </Typography>
-          </Button>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Button
-              component={Link}
-              to="/bahn"
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Bahn
-            </Button>
-            <Button
-              component={Link}
-              to="/mensa"
-              sx={{ my: 2, color: "white", display: "block" }}
-            >
-              Mensa
-            </Button>
-          </Box>
-
-          {/*           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
-        </Toolbar>
-      </Container>
+    <AppBar position="static" sx={{ backgroundColor: "#6FD95D" }}>
+      <Toolbar disableGutters alignItems="center" justifyContent="flex-end" sx={{right: "25px"}}>
+        <Stack direction="row" spacing={2}>
+          <Avatar
+            alt="Mensa"
+            src="https://i.imgur.com/JLW4tyw.png"
+            component={Link}
+            to="/mensa"
+          />
+          <Avatar
+            alt="Bahn"
+            src="https://i.imgur.com/nCDqp66.png"
+            component={Link}
+            to="/bahn"
+          />
+          <Avatar
+            alt="Events"
+            src="https://i.imgur.com/66iKABg.png"
+            component={Link}
+            to="/"
+          />
+        </Stack>
+      </Toolbar>
     </AppBar>
   )
 }
