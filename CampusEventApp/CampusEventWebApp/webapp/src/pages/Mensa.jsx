407cf35f8dd4b975e23b3d422947d71644ebb748
Will Kupec <willem.kupec@gmail.com>
2024-07-01 21:05:20 +0200

    [mensa] working on page design

--- CampusEventApp/CampusEventWebApp/webapp/src/pages/Mensa.jsx
+++ CampusEventApp/CampusEventWebApp/webapp/src/pages/Mensa.jsx
@@ -1,10 +1,35 @@
 import Header from "../components/Header"
+import { Grid, Paper, Typography } from "@mui/material"
 
 const Mensa = () => {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Grid container padding={2} paddingBottom={0}>
+        <Grid container item direction="column" rowSpacing={2} xs={3}>
+          <Grid item>
+            <Paper
+              component="img"
+              alt="htw_logo"
+              src="https://i.imgur.com/K1Jc19l.png"
+              width="400px"
+              borderRadius="10px"
+              elevation={5}
+            ></Paper>
+          </Grid>
+          <Grid item>
+            <Paper
+              component="img"
+              alt="campus_banner"
+              src="https://i.imgur.com/omsunDN.jpg"
+              width="400px"
+              borderRadius="10px"
+              elevation={5}
+            ></Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
